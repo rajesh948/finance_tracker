da3971825cc9d0b2e373ffc036625cd2e11853bf
rajesh948 <rajeshparmar6745@gmail.com>
2023-06-09 18:46:33 +0530

    adding file for deployment in netlify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/],
+    },
+  },
+};
